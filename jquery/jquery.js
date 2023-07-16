c108c8924b78edac64cd1b61aebd5cfa5f3d9fed
omar4real <alahmadomar093@gmail.com>
2023-07-16 18:00:48 +0300

    idk

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -7,13 +7,21 @@ $(document).ready(function () {
             let position = $(this).css("left");
             let po = parseInt(position.substring(0,position.length-2));
             
-            if (po >= window.innerWidth - 50) {
+            if (po >= window.innerWidth - 100) {
                 $(this).css({
                     left: "0px",
                     top: "+=100px"
                 });
             }
-                    
+            let hieght = $(this).css("top");
+            let h = parseInt(hieght.substring(0,hieght.length-2));
+            
+            if (h >= window.innerHeight - 200) {
+                $(this).css({
+                    left: "0px",
+                    top: "0px"
+                });
+            }        
     });
 });
 
